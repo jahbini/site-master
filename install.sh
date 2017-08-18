d6974f8aeb9daf00bfba1874d5c2f570a4b1daa7
James <jim@jahbini.org>
2017-08-18 04:15:09 +0000

    update names of repositories site-loader and site-server

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@
 mkdir mongodb -p
 mkdir sites -p
 
-scp root@139.59.124.175:/site-master/mongodb.tgz .
+scp -i /root/.ssh/site-master root@139.59.124.175:/site-master/mongodb.tgz .
 tar xfz mongodb.tgz
 
-git clone http://github.com/jahbini/site-master site-loader
+git clone https://github.com/jahbini/site-loader.git
 for i in stjohnsjim celarien bamboosnow
 do
-( cd sites ; git clone http://github.com/jahbini/$i )
+( cd sites ; git clone https://github.com/jahbini/$i )
 ( cd site-loader; mkdir domains -p; ln -s ../sites/$i domains/$i; ln -s ../public-$i ./public-$i)
 done
 
-git clone http://github.com/jahbini/snowserv site-server
+git clone https://github.com/jahbini/site-server.git
 
 (cd site-server; docker build -t site-server:latest . )
 
