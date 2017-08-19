227964e3649b689388747fff2bc90550b787bca7
root <jahbini@jahbini.org>
2017-08-19 03:36:00 +0000

    add image from source, fix links

--- install.sh
+++ install.sh
@@ -4,21 +4,28 @@
 
 mkdir mongodb -p
 mkdir sites -p
+git clone https://github.com/jahbini/site-server.git
+git clone https://github.com/jahbini/site-loader.git
+
+scp -i /root/.ssh/site-master root@138.68.249.239:/site-master/images.tgz .
+tar xfz images.tgz
 
 scp -i /root/.ssh/site-master root@139.59.124.175:/site-master/mongodb.tgz .
 tar xfz mongodb.tgz
 
-git clone https://github.com/jahbini/site-loader.git
 for i in stjohnsjim celarien bamboosnow
 do
-( cd sites ; git clone https://github.com/jahbini/$i )
-( cd site-loader; mkdir domains -p; ln -s ../sites/$i domains/$i; ln -s ../public-$i ./public-$i)
+mkdir /site-master/public-$i
+( cd sites ; git clone https://github.com/jahbini/$i.git )
+( cd site-loader; mkdir domains -p;
+ ln -s /site-master/sites/$i domains/$i;
+ ln -s /site-master/public-$i ./public-$i)
 done
-
+npm install -g brunch
 (cd site-loader; npm install)
-git clone https://github.com/jahbini/site-server.git
+(cd site-loader; bash brunch-devo)
 
 (cd site-server; docker build -t site-server:latest . )
 
-. ./go.sh
+. ./up.sh
 
