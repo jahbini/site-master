fd0bfa0534ef68c68ac6a14d899092175ce677b5
root <jahbini@jahbini.org>
2017-09-04 00:57:50 +0000

    add script to convert directory from js to coffescipt

--- scripts/caffine.sh
+++ scripts/caffine.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 #
 # convert javascript into coffeescript
-mkdir ./javascript
+mkdir ./javascript 2>0-problems
 for i in *.js
 do
+  echo "---Converting $i to $j.coffee---" >>0-problems
   j=`basename $i .js`
-  js2coffee $i $j.coffee &2>$j.probs
+  js2coffee $i >$j.coffee 2>>0-problems
   mv $i ./javascript/$i
 done
