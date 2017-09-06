3bb57df08020d8979297eb77f8505269622ef812
root <jahbini@jahbini.org>
2017-09-06 00:14:00 +0000

    correct ordering inside caffine

--- scripts/caffine.sh
+++ scripts/caffine.sh
@@ -4,8 +4,8 @@
 mkdir ./javascript 2>0-problems
 for i in *.js
 do
-  echo "---Converting $i to $j.coffee---" >>0-problems
   j=`basename $i .js`
+  echo "---Converting $i to $j.coffee---" >>0-problems
   js2coffee $i >$j.coffee 2>>0-problems
   mv $i ./javascript/$i
 done
