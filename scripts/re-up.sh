e55e382218bccc16dfe2aabbb176dfb12137b314
root <jahbini@jahbini.org>
2018-02-24 20:09:00 +0000

    force mongo restart, fix paths on re-up

--- scripts/re-up.sh
+++ scripts/re-up.sh
@@ -4,10 +4,10 @@
 
 sysctl vm.vfs_cache_pressure=10
 sysctl vm.swappiness=10
-(cd site-loader; docker-compose down )
+(cd /site-master; docker-compose down )
 #(cd site-loader; tar cfz mongodb.tgz mongodb )
-(cd site-loader; bash brunch-devo.sh )
-(cd site-server; docker build -t site-server:latest . )
+(cd /site-master/site-loader; bash brunch-devo.sh )
+(cd /site-master/site-server; docker build -t site-server:latest . )
 
 (nohup docker-compose up nginx db >engines.log &)
 sleep 10
